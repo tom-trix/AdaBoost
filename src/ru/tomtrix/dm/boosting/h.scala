7f7e4d3eee2e993db4917890f0d1394240c97e5f
tommy <tom-trix@yandex.ru>
2013-01-01 21:02:50 +0600

    Чё-то, видимо, изменил

--- src/ru/tomtrix/dm/boosting/h.scala
+++ src/ru/tomtrix/dm/boosting/h.scala
@@ -1,72 +1,43 @@
 package ru.tomtrix.dm.boosting
 
+import scala.Math._
+
 /**
- * Weak classificator that uses simple stump-function: IF (X(k) > Θ) => 1 ELSE -1
+ * Strong classificator obtained by Ada-Boost algorithm
+ * @param trainingSet - training set to learn
+ * @param n - number od iterations
  * @author tom-trix
  */
-trait h {
-    /** Which component of vector to check */
-    val k: Int
-    /** Treshold (this is Tetta! Not Zero!) */
-    val Θ: Double
+class H(trainingSet: Map[X, Int]) {
+    //weights (initially = 1/n)
+    private var D = trainingSet.toList map {t => 1.0 / trainingSet.size}
+    //calculate a sequence of ("α"; "h(x)") pairs
+    private val result = for {
+        i <- 1 to 16 map (t => {
+            //find the best weak classificator and its an ε-error
+            val (ht, εt) = h find (trainingSet, D)
+            if (εt < 0.5) {
+                //find α(t)
+                val αt = 0.5 * log((1 - εt) / εt)
+                //recalculate weights (= D(i)*exp(-αyh(x)))
+                D = D zip trainingSet.toList map (d => d._1 * exp(-αt * d._2._2 * ht(d._2._1)))
+                //normalize them
+                val n = D.sum
+                D = D map {_ / n}
+                //print
+                println("%2d): %52s; ε=%.18f; α=%.18f" format (t, ht, εt, αt))
+                Some(αt, ht)
+            } else None
+        })
+    } yield i
+    
     /**
-     * @param t - input vector
+     * @param x - input vector
      * @return classification result (+1 or -1)
      */
-    def apply(t: X) = if (t.items(k) > Θ) 1 else -1
-}
-
-/**
- * Weak classificator that uses modifyed stump-function: IF "n" components > Θ => 1 ELSE -1
- * @author tom-trix
- */
-trait multiStump extends h {
-    override val k = 0;
-    override val Θ = 0d;
-    /** Which components of vector to check */
-    val components: Set[Int]
-    /** List of corresponding tresholds */
-    val tresholds: Seq[Double]
-    /** Function to be applied (exists or forall) */
-    val m: Int
-    override def apply(t: X) = {
-        val lst = for {
-            c <- components toList
-        } yield t.items(c)
-        if (lst.zip(tresholds).toList.filter(p => p._1 > p._2).size >= m) 1 else -1
-    }
-}
-
-/**
- * Object to deal with weak classificators
- * @author tom-trix
- */
-object h {
-    /** List of weak classificators */
-    private var allClassificators: List[h] = List()
-
-    /**
-     * Adds new weak classificator
-     * @param t
-     */
-    def addClassificators(t: List[h]) = allClassificators = t ::: allClassificators
-
-    /**
-     * @param w - weak classificator
-     * @param trainingSet - training set to learn
-     * @param D - current weights
-     * @return ε-error (= summa of such D(i) where y(i) != h(x(i)))
-     */
-    private def ε(w: h, trainingSet: Map[X, Int], D: Seq[Double]) =
-        trainingSet.zip(D).map { p => if (p._1._2 != w(p._1._1)) p._2 else 0 }.toList sum;
-
-    /**
-     * @param trainingSet - training set to learn
-     * @param D - current weights
-     * @return (ε, h)-pair where "h" is the most appropriate weak classificator that corresponds to the current weights; "ε" is the error
-     */
-    def find(trainingSet: Map[X, Int], D: Seq[Double]) = {
-        val min_h = h.allClassificators.minBy(t => ε(t, trainingSet, D))
-        (min_h, ε(min_h, trainingSet, D))
+    def apply(x: X) = {
+        val s = result.flatten.map { t => t._1 * t._2(x) }.sum
+        println ("summa = " + s)
+        s
     }
 }
